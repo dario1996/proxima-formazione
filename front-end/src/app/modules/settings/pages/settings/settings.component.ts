3579422ccd16235ba9ed24ffd31a2dabccdf3c9b
Dario Biffero <dariobiffero@gmail.com>
2025-06-24 17:12:54 +0200

    bozza implementazione settings

--- front-end/src/app/modules/settings/pages/settings/settings.component.ts
+++ front-end/src/app/modules/settings/pages/settings/settings.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
 
+  selectedTab = 'piattaforme';
 }
