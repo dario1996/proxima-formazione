65e4d2c344c12d7f4f1823385a46070c7d1bdc9f
frogstomp1987 <frogstomp1987@yahoo.it>
2025-06-25 10:23:57 +0200

    Modifica titolo per componente corsi

--- front-end/src/app/modules/corsi/pages/corsi/corsi.component.ts
+++ front-end/src/app/modules/corsi/pages/corsi/corsi.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PageTitleComponent } from '../../../../core/page-title/page-title.component';
 
 @Component({
   selector: 'app-corsi',
+  imports: [PageTitleComponent],
   templateUrl: './corsi.component.html',
   styleUrls: ['./corsi.component.css'],
   standalone: true
@@ -9,6 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class CorsiComponent implements OnInit {
 
   constructor() { }
+
+  title: string = 'Corsi';
+  icon: string = 'fa-solid fa-book';
 
   ngOnInit(): void {
   }
