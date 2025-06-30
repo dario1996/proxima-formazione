240ae0d44ec71fcbbe85381f38ec4f05965a16e1
Dario Biffero <dariobiffero@gmail.com>
2025-06-30 17:52:39 +0200

    smal fix

--- front-end/src/app/core/page-title/page-title.component.ts
+++ front-end/src/app/core/page-title/page-title.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-page-title',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './page-title.component.html',
   styleUrl: './page-title.component.css'
 })
 export class PageTitleComponent {
   @Input() title: string = '';
   @Input() subtitle: string = '';
+  @Input() showButton: boolean = true; // <--- aggiunto
 
   @Output() nuovoCorso = new EventEmitter<void>();
 }
