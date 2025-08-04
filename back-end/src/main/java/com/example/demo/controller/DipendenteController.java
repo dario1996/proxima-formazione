33721cf06f409ebc7dea46c7fca6aebff6e0f103
Simone Mazza <simone.mazza@ext.myefm.it>
2025-08-04 15:28:17 +0200

    Refactor bulk import services package; update controllers and configuration
    - Renamed AssegnazioneBulkImportService and DipendenteBulkImportService to 'services' package
    - Updated AssegnazioneController and DipendenteController
    - Modified application.yml configuration

--- back-end/src/main/java/com/example/demo/controller/DipendenteController.java
+++ back-end/src/main/java/com/example/demo/controller/DipendenteController.java
@@ -5,7 +5,7 @@ import com.example.demo.dto.DipendenteBulkImportRequest;
 import com.example.demo.dto.DipendenteBulkImportResponse;
 import com.example.demo.entity.Dipendente;
 import com.example.demo.repository.DipendenteRepository;
-import com.example.demo.service.DipendenteBulkImportService;
+import com.example.demo.services.DipendenteBulkImportService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
--- back-end/src/main/java/com/example/demo/services/AssegnazioneBulkImportService.java
+++ back-end/src/main/java/com/example/demo/services/AssegnazioneBulkImportService.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.services;
 
 import com.example.demo.dto.AssegnazioneBulkImportItem;
 import com.example.demo.dto.AssegnazioneBulkImportRequest;
